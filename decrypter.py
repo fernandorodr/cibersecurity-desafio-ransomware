3801ee880af6775f5e99541653681335531d4b4d
fernandorodr <40827174+fernandorodr@users.noreply.github.com>
2023-11-16 11:38:55 -0300

    Melhoria no decrypter.py
    Alterado o código para selecionar o arquivo

--- decrypter.py
+++ decrypter.py
@@ -1,22 +1,40 @@
 import os
 import pyaes
+from tkinter import Tk, filedialog
 
-## abrir o arquivo criptografado
-file_name = "teste.txt.ransomwaretroll"
-file = open(file_name, "rb")
-file_data = file.read()
-file.close()
+def decrypt_file(file_path, key):
+    try:
+        with open(file_path, "rb") as file:
+            file_data = file.read()
 
-## chave para descriptografia
-key = b"testeransomwares"
-aes = pyaes.AESModeOfOperationCTR(key)
-decrypt_data = aes.decrypt(file_data)
+            aes = pyaes.AESModeOfOperationCTR(key)
+            decrypt_data = aes.decrypt(file_data)
 
-## remover o arquivo criptografado
-os.remove(file_name)
+            decrypted_file_path = os.path.splitext(file_path)[0] + "_decrypted.txt"
+            with open(decrypted_file_path, "wb") as new_file:
+                new_file.write(decrypt_data)
 
-## criar o arquivo descriptografado
-new_file = "teste.txt"
-new_file = open(f'{new_file}', "wb")
-new_file.write(decrypt_data)
-new_file.close()
+            print(f"Arquivo descriptografado salvo como: {decrypted_file_path}")
+            return decrypted_file_path
+    except Exception as e:
+        print(f"Ocorreu um erro durante a descriptografia: {e}")
+        return None
+
+def main():
+    root = Tk()
+    root.withdraw()
+    file_path = filedialog.askopenfilename(title="Selecione o arquivo criptografado para descriptografar")
+
+    if file_path:
+        key = b"testeransomwares"  # Sua chave de descriptografia
+
+        decrypted_file = decrypt_file(file_path, key)
+        if decrypted_file:
+            try:
+                os.remove(file_path)
+                print(f"Arquivo criptografado removido: {file_path}")
+            except Exception as e:
+                print(f"Não foi possível remover o arquivo criptografado: {e}")
+
+if __name__ == "__main__":
+    main()
